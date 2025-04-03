2caafe6a1ddce35c3206c55ed0a6dc16cc4250b8
Sameer Chauhan <sameerchauhan212204@gmail.com>
2025-04-03 19:50:46 +0530

    render

--- Job_Tracker/settings.py
+++ Job_Tracker/settings.py
@@ -82,12 +82,17 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SQLite Database Configuration
+import os
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True  # Enforce SSL for security
+    )
 }
+
 
 
 # Password validation
